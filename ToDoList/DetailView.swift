70e5bcd46b904b2580082eb926af178face930c2
Alessandro Gaspari <alessandrogaspari48@gmail.com>
2025-07-26 19:41:13 +0200

    Con aggiornamenti

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Alessandro Gaspari on 26/07/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}

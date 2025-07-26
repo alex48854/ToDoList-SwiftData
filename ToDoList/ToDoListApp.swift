4bc6c8b9d9ba7cd2350a05359dd2a78fe8b383f8
Alessandro Gaspari <alessandrogaspari48@gmail.com>
2025-07-26 17:35:32 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Alessandro Gaspari on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
